cce0b3e536763798e449cae23bd8607bbdd8a07f
Smeet Parmar <77852984+StaticGamerYT-India@users.noreply.github.com>
2025-05-12 23:47:18 +0530

    Delete src directory

--- src/main.tsx
+++
@@ -1,8 +0,0 @@
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
-
-// Import Material Web components
-import './components/material';
-
-createRoot(document.getElementById("root")!).render(<App />);
